2477934b158466f95ba54318a34dd9f927663ec1
james <j.alsop99@hotmail.com>
2022-01-07 14:52:16 +0000

    Deployment config

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ja2500capstone.eu-gb.mybluemix.net"]
 
 
 # Application definition
